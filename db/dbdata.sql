bc11cc015e51893a64aeced19133e21712e0de73
zack.ogoma <zack.ogoma@turnkeyafrica.com>
2021-10-06 06:50:42 +0300

    Including fyway migration

---
+++ db/dbdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO dad2qrd64dqac5.farm_section (section_name,id) VALUES('operations', 302);
+INSERT INTO dad2qrd64dqac5.farm_section (section_name,id) VALUES('manager', 303);
+INSERT INTO dad2qrd64dqac5.farm_section (section_name,id) VALUES('accountant', 304);
